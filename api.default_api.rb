3c041b6b76f09c5bc8627cf18865fafc23c4790d
Yasuhiro ABE <yasu-abe@u-aizu.ac.jp>
2024-11-11 10:21:39 +0900

    Fixed the @param_rows and @param_start issue of the api.default_api.rb file.

--- api.default_api.rb
+++ api.default_api.rb
@@ -40,7 +40,9 @@ MyApp.add_route('GET', '/search', {
   require 'httpclient'
   client = HTTPClient.new
   @param_q = params.has_key?(:q) ? params[:q].to_str  : "sccp"
-  url = URI::HTTPS.build({:host => "opm00h.u-aizu.ac.jp", :path => '/solr/api/v1/search', :query => "q=#{@param_q}&wt=json&site="})
+  @param_rows = params.has_key?(:rows) ? params[:rows].to_i : 10
+  @param_start = params.has_key?(:start) ? params[:start].to_i : 0
+  url = URI::HTTPS.build({:host => "opm00h.u-aizu.ac.jp", :path => '/solr/api/v1/search', :query => "q=#{@param_q}&rows=#{@param_rows}&start=#{@param_start}&wt=json&site="})
   ret = client.get(url)
   @result = JSON.parse(ret.body)
   output = erb :header
